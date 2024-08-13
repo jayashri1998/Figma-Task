d4819e37627fe1a41349aeee7e6c15c3dfa4d95a
Shadbox21 <hr@shadbox.com>
2024-08-13 08:29:23 +0530

    changes done

--- src/Components/Home/Herosection.jsx
+++ src/Components/Home/Herosection.jsx
@@ -1,28 +1,21 @@
-import React from 'react'
+import React from "react";
 
-
-
-
-import MainHerosection from './MainHerosection'
-import ContactUs from './ContactUs'
-import Card from './Card'
-import HalalService from './HalalService'
-import SocialService from './SocialService'
+import MainHerosection from "./MainHerosection";
+import ContactUs from "./ContactUs";
+import Card from "./Card";
+import HalalService from "./HalalService";
+import SocialService from "./SocialService";
 const Herosection = () => {
-
   return (
-    <div className=' px-3 py-3 lg:px-[100px] lg:py-[140px] font-montserrat leading-6'>
-    <div className='lg:inline-flex flex-col items-start gap-[120px]'> 
-  
-  <MainHerosection/>
-    </div>
-    <Card/>
-    <HalalService/>
-    <SocialService/>
-    <ContactUs/>
-  
-    </div>
-  )
-}
+    <div className=" px-3 py-3 lg:px-[100px] lg:py-[140px] font-montserrat leading-6">
+      <MainHerosection />
 
-export default Herosection
+      <Card />
+      <HalalService />
+      <SocialService />
+      <ContactUs />
+    </div>
+  );
+};
+
+export default Herosection;
--- src/Components/Home/MainHerosection.jsx
+++ src/Components/Home/MainHerosection.jsx
@@ -3,35 +3,37 @@ import img from '../../assets/establised.svg';
 
 const MainHerosection = () => {
   return (
-    <div className="relative flex flex-wrap sm:justify-center sm:flex-nowrap w-full space-y-4">
-      <div className="max-w-[85rem] w-full mx-auto sm:flex sm:items-center sm:justify-between">
-        <div className="flex  items-center justify-between lg:gap-[128px]">
-          {/* Text section */}
-          <div className="order-2 lg:order-1 flex flex-col lg:w-[517px] items-start">
-            <h2
-              className="text-[#005447] text-[32px] lg:text-[56px] font-bold tracking-[1.12px] pl-3 lg:pl-0"
-              style={{ lineHeight: 'normal' }}
-            >
-              Halal Certification & Compliance
-            </h2>
-            <p className="text-[#313131] pl-3 lg:pl-0 pt-3 text-xs lg:text-lg lg:pt-[16px] font-semibold lg:font-medium lg:leading-[24px] tracking-[3.6px] uppercase whitespace-nowrap flex space-x-1 lg:space-x-2">
-              <span>STAY CLEAN</span>
-              <span className="px-1 lg:px-1">|</span>
-              <span>EAT GOOD</span>
-              <span className="px-1 lg:px-1">|</span>
-              <span>LIVE BETTER</span>
-            </p>
-            <div className="pt-6 lg:pt-[32px] text-[#FFF] text-[14px] font-semibold leading-[24px]">
-              <button className="flex px-[32px] py-[12px] justify-center items-center gap-[10px] rounded-[12px] border-[1px] border-[#005447] bg-[#008F78]">
-                View all services
-              </button>
-            </div>
-          </div>
+    
+    <div className="flex flex-col lg:flex-row  items-center space-y-4 lg:gap-40">
+      {/* Image section */}
+      <div className="order-1 lg:order-2 w-full lg:w-[500px] lg:h-[322px] shrink-0 lg:pl-8">
+        <img src={img} alt="img" className="w-full h-full object-cover" />
+      </div>
 
-          {/* Image section */}
-          <div className="order-1 lg:order-2 flex flex-col lg:pt-3 lg:flex-row lg:justify-between gap-6 lg:gap-[128px]">
-            <img src={img} alt="img" className="w-full h-full object-cover" />
-          </div>
+      {/* Text section */}
+      <div className="order-2 lg:order-1 lg:flex flex-col  lg:w-[517px] items-start -mb-1">
+        <h2
+          className="w-[268px] lg:w-[447px] pl-3 lg:pl-0 lg:h-[207px] text-[#005447]  text-[32px] lg:text-[56px] font-bold tracking-[1.12px]"
+          style={{ lineHeight: 'normal' }}
+        >
+          Halal Certification & Compliance
+        </h2>
+        <p className="text-[#313131] pl-3 lg:pl-0 pt-3 text-xs lg:text-lg lg:pt-[16px] font-semibold lg:font-medium lg:leading-[24px] tracking-[3.6px] uppercase whitespace-nowrap flex space-x-1 lg:space-x-2">
+        <span>STAY CLEAN</span>
+        <span className="px-1 lg:px-1"> {/* Adjust px for spacing around the separator */}
+          |
+        </span>
+        <span>EAT GOOD</span>
+        <span className="px-1 lg:px-1"> {/* Adjust px for spacing around the separator */}
+          |
+        </span>
+        <span>LIVE BETTER</span>
+      </p>
+      
+        <div className="flex flex-col pl-3 lg:pl-0 items-start pt-6 lg:pt-[32px] text-[#FFF] text-[14px] font-semibold leading-[24px]">
+          <button className="flex px-[32px] py-[12px] justify-center items-center gap-[10px] rounded-[12px] border-[1px] border-[#005447] bg-[#008F78]">
+            View all services
+          </button>
         </div>
       </div>
     </div>
