e895f5ed9d3d21856ea99dae98381675d011bdb7
Shadbox21 <hr@shadbox.com>
2024-08-17 18:21:33 +0530

    change side width

--- src/Components/OurService/OurProcess.jsx
+++ src/Components/OurService/OurProcess.jsx
@@ -10,7 +10,7 @@ const Ourprocess = () => {
       <div className='flex flex-col lg:flex-row lg:justify-between gap-6 lg:gap-[50px] mt-6 lg:mt-10'>
         
         {/* Image Section */}
-        <div className='order-1 lg:order-2 flex justify-center items-center w-full lg:w-[50%]'>
+        <div className='order-1 lg:order-2 flex justify-center items-center w-full lg:w-[30%]'>
           <img src={img5} alt='Halal Service' className='w-full h-auto rounded-xl' />
         </div>
 
@@ -63,7 +63,7 @@ const Ourprocess = () => {
               <li>Packaging and labeling</li>
             </ul>
           </div>
-          <p className='pt-6 font-normal text-base text-justify text-[#2A422B]'>
+          <p className='pt-6 font-normal text-base  xl:text-lg 2xl:text-xl text-justify text-[#2A422B]'>
             During the audit, the business might have to provide criteria of acceptance of raw materials (ingredients), certificate of analysis, and Halal certificate of individual ingredients. Once the audit is completed, an audit report will be prepared and signed by both parties.
           </p>
         </div>
@@ -74,7 +74,7 @@ const Ourprocess = () => {
             <span className='font-medium text-lg lg:leading-[24px] tracking-[2.5px] uppercase text-[#646464]'>Step 3</span>
             <p className='text-[#005447] font-bold text-xl lg:text-2xl'>Certification</p>
           </h2>
-          <p className='pt-6 font-normal text-base text-justify text-[#2A422B]'>
+          <p className='pt-6 font-normal text-base  xl:text-lg 2xl:text-xl text-justify text-[#2A422B]'>
             Once the Halal audit is complete, a Technical Committee will review the documents submitted by the business and the audit report submitted by the Auditors. If the audit report is satisfactory and the business and/or products satisfy the Halal certification criteria, then the Halal Certification Body issues the Halal Certificate.
           </p>
         </div>
